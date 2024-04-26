4f0233b72105a9146919d501e9c8fcd5827ae79c
jayant13-03-05 <jayantsaxena1303@gmail.com>
2024-04-26 17:18:28 +0530

    hhhh

--- ecom-api/models/product.js
+++ ecom-api/models/product.js
@@ -1,58 +1,62 @@
 const mongoose = require('mongoose');
-const Schema =mongoose.Schema({
-    name:{
-        type:String,
-        required:true,
-        minLength:3,
-        maxLength:20,
+const Schema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        minLength: 3,
+        maxLength: 20,
     },
-    description:{
-        type:String,
-        required:true,
+    description: {
+        type: String,
+        required: true,
     },
-    richDescription:{
-        type:String,
-        required:false,
+    richDescription: {
+        type: String,
+        required: false,
     },
-    Image:String,
-    images:[{
-        type:String,
+    Image: String,
+    images: [{
+        type: String,
     }],
-    brand:{
-        type:String,
-        default:''
+    brand: {
+        type: String,
+        default: ''
     },
-    price:{
-        type:String,
-        default:0
+    price: {
+        type: String,
+        default: 0
     },
-    category:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:'Category',
-        required:true,
+    category: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Category',
+        required: true,
     },
-    countInStock:{
-        type:Number,
-        required:true,
-        min:0,
-        max:255,
+    countInStock: {
+        type: Number,
+        required: true,
+        min: 0,
+        max: 255,
     },
-    rating:{
-        type:String,
-        default:0,
+    rating: {
+        type: String,
+        default: 0,
     },
-    numReviews:{
-        type:Number,
-        default:0,
+    numReviews: {
+        type: Number,
+        default: 0,
     },
-    isFeatured:{
-        type:String,
-        default:false,
+    isFeatured: {
+        type: String,
+        default: false,
     },
-    dateCreated:{
-        type:String,
-        default:Date.now,
+    dateCreated: {
+        type: String,
+        default: Date.now,
     },
 })
 
-module.exports =mongoose.model('Product',Schema);
+module.exports = mongoose.model('Product', Schema);
+
+{
+
+}
