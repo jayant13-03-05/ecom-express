d494fc882d26ea9a88d334533d778c71703fa618
jayant13-03-05 <jayantsaxena1303@gmail.com>
2024-05-11 13:45:29 +0530

    hhvghv

--- ecom-api/routes/user.js
+++ ecom-api/routes/user.js
@@ -5,19 +5,20 @@ var router = express.Router();
 const bcrypt = require('bcryptjs');
 
 
-router.post('/', async (req,res)=>{
-    let user =new user({
-        name:req.body.name,
-        email:req.body.email,
-        passwordHash:bcrypt.hashSync(req.body.password,10),
-        phone:req.body.phone,
-        isAdmin:req.body.isAdmin,
-        street:req.body.street,
-        apartment:req.body.apartment,
-        zip:req.body.zip,
-        city:req.body.city,
-        country:req.body.country,
-    })
+router.post('/test', async (req, res) => {
+  let user = new user({
+    name: req.body.name,
+    email: req.body.email,
+    passwordHash: bcrypt.hashSync(req.body.password, 10),
+    phone: req.body.phone,
+    isAdmin: req.body.isAdmin,
+    street: req.body.street,
+    apartment: req.body.apartment,
+    zip: req.body.zip,
+    city: req.body.city,
+    country: req.body.country,
+  })
+  user = await user.save();
 })
 
 
