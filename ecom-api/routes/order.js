c5be2dce2364150f7bad3f49f2bc790a78243340
jayant13-03-05 <jayantsaxena1303@gmail.com>
2024-05-31 17:11:57 +0530

    nbhbgh

--- ecom-api/routes/order.js
+++ ecom-api/routes/order.js
@@ -4,29 +4,29 @@ const model = require('../models/order-item')
 var router = express.Router();
 
 /* add model to list. */
-router.post('/add', async(req, res) => {
-  const orderItemIds= Promise.all(req.body.OrderItem.map(async orderItem =>{
+router.post('/add', async (req, res) => {
+  const orderItemIds = Promise.all(req.body.orderItem.map(async orderItem => {
     let newOrderItem = new model({
-      quantity:orderItem.quantity,
-      product:orderItem.product
+      quantity: orderItem.quantity,
+      product: orderItem.product
     })
     newOrderItem = await newOrderItem.save();
     return newOrderItem._id;
   }))
-   const OrderItemIds2 = await orderItemIds;
-   console.log(orderItemIds);
+  const OrderItemIds2 = await orderItemIds;
+  console.log(orderItemIds);
 
-  let obj=new Model ({
-    OrderItem:OrderItemIds2,
-    shippingAddress1:req.body.shippingAddress1,
-    shippingAddress2:req.body.shippingAddress2,
-    city:req.body.city,
-    zip:req.body.zip,
-    country:req.body.country,
-    phone:req.body.phone,
-    status:req.body.status,
-    totalPrice:req.body.totalPrice,
-    user:req.body.user,
+  let obj = new Model({
+    OrderItem: OrderItemIds2,
+    shippingAddress1: req.body.shippingAddress1,
+    shippingAddress2: req.body.shippingAddress2,
+    city: req.body.city,
+    zip: req.body.zip,
+    country: req.body.country,
+    phone: req.body.phone,
+    status: req.body.status,
+    totalPrice: req.body.totalPrice,
+    user: req.body.user,
   })
   const insertedObj = await obj.save();
   console.log(insertedObj);
@@ -34,31 +34,32 @@ router.post('/add', async(req, res) => {
 });
 
 /* get all customer. /student/find */
-router.get('/find', async(req, res) => {
+router.get('/find', async (req, res) => {
   const list = await Model.find();
   return res.status(201).json(list);
 });
 
 
 /* get all customer. /student/find/12345 */
-router.get('/find/:id', async(req, res) => {
+router.get('/find/:id', async (req, res) => {
   const list = await Model.findById(req.params.id);
   return res.status(201).json(list);
 });
 
 /* get all category. /student/find/12345 */
-router.delete('/remove/:id', async(req, res) => {
+router.delete('/remove/:id', async (req, res) => {
   const list = await Model.findByIdAndDelete(req.params.id);
-  return res.status(201).json({msg:"data deleted sucessfully"});
+  return res.status(201).json({ msg: "data deleted sucessfully" });
 });
 
 /* add category list. */
-router.put('/update/:id', async(req, res) => {
-  let obj=req.body;
+router.put('/update/:id', async (req, res) => {
+  let obj = req.body;
   const insertobj = await Model.findOneAndUpdate({
-    _id:req.params.id},obj,{new:true});
+    _id: req.params.id
+  }, obj, { new: true });
   return res.status(201).json(insertobj);
-  
+
 });
 
 
